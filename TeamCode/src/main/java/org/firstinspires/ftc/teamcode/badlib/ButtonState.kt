ca8f90f2efc3db8dcfbdc2513c7a34ebb51c6a44
Andrew Emmel <andy.emmel@gmail.com>
2019-11-20 08:29:01 -0800

    aaaaaaaaaaaaaaa

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/badlib/ButtonState.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/badlib/ButtonState.kt
@@ -1,5 +1,18 @@
 package org.firstinspires.ftc.teamcode.badlib
 
+class Button() {
+
+    var state: ButtonState = ButtonState.NOT_PRESSED
+        private set(value) {
+            state = value
+        }
+
+    fun update(value: Boolean): ButtonState {
+        state = state.update(button = value)
+        return state
+    }
+}
+
 enum class ButtonState {
     NOT_PRESSED,
     PRESSED,
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/bad/auto/CenterDrive.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/bad/auto/CenterDrive.kt
@@ -4,6 +4,8 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.util.ElapsedTime
 import org.firstinspires.ftc.teamcode.badlib.AutoDriveCommand
+import org.firstinspires.ftc.teamcode.badlib.Button
+import org.firstinspires.ftc.teamcode.badlib.ButtonState
 import org.firstinspires.ftc.teamcode.badlib.MecanumDrivetrain
 
 @Autonomous(name = "Drive to Center", group = "bad")
@@ -12,21 +14,29 @@ class CenterDrive: LinearOpMode() {
     val drivetrain: MecanumDrivetrain = MecanumDrivetrain(hardwareMap)
     val runtime: ElapsedTime = ElapsedTime()
 
-    val redCommands = arrayOf<AutoDriveCommand>(
+    val leftCommands = arrayOf<AutoDriveCommand>(
 
     )
 
-    val blueCommands = arrayOf<AutoDriveCommand>(
+    val rightCommands = arrayOf<AutoDriveCommand>(
 
     )
 
     override fun runOpMode() {
 
+        drivetrain.init()
+        val commands = selectionPrompt()
 
+        waitForStart()
+        runtime.reset()
+        commands.forEach {
+            val done = drivetrain.setAutoDrive(it)
+            while (done.invoke()) {}
+        }
 
     }
 
-    fun selectionPrompt() {
+    fun selectionPrompt(): Array<AutoDriveCommand> {
         val intro = """
             USE THE LEFT AND RIGHT DPAD BUTTONS FOR SELECTION
             =================================================
@@ -34,9 +44,20 @@ class CenterDrive: LinearOpMode() {
             RIGHT: GO RIGHT
         """.trimIndent()
 
+        val left = Button()
+        val right = Button()
+
         telemetry.clear()
         telemetry.addLine(intro)
         telemetry.update()
+
+        while (left.update(gamepad1.dpad_left) == ButtonState.NOT_PRESSED && right.update(gamepad1.dpad_right) == ButtonState.NOT_PRESSED) {}
+
+        telemetry.clear()
+        telemetry.update()
+
+        if (left.state != ButtonState.NOT_PRESSED) return leftCommands
+        else return rightCommands
 
     }
 
